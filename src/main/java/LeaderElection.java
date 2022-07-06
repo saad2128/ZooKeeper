1f2b842083faccb5815d82a9cdd1dd73d7b5bf85
Saad Aslam <saslam01@i2cinc.com>
2022-07-06 17:06:06 +0500

    ZooKeeper Distributed Systems

--- src/main/java/LeaderElection.java
+++ src/main/java/LeaderElection.java
@@ -1,47 +1,79 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderElection implements Watcher {
 
     private static final String ZOOKEEPER_ADDRESS="localhost:2181";
     private static final int SESSION_TIMEOUT=3000;
+    private static final String ELECTION_NAMESPACE="/election";
+    private String currentZnodeName;
     private ZooKeeper zooKeeper;
 
-    public static void main(String[] args) throws IOException,InterruptedException{
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection=new LeaderElection();
         leaderElection.connectToZooKeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
         leaderElection.run();
         leaderElection.close();
-
+        System.out.println("Disconnect from application");
     }
 
-    public void connectToZooKeeper() throws  IOException{
-        this.zooKeeper=new ZooKeeper(ZOOKEEPER_ADDRESS,SESSION_TIMEOUT, this);
+    public void connectToZooKeeper() throws IOException{
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
     }
 
-    public void run() throws InterruptedException{
-
-        synchronized(zooKeeper){
+    public void run() throws InterruptedException {
+        synchronized (zooKeeper) {
             zooKeeper.wait();
         }
     }
-    public void process(WatchedEvent event){
-         switch (event.getType()){
-             case None:
-                 if(event.getState() == Watcher.Event.KeeperState.SyncConnected){
-                     System.out.println("Successfully connected");
-                 }
-                 else{
-                     synchronized (zooKeeper){
-                         zooKeeper.notifyAll();
-                     }
-                 }
-         }
-    }
-    public void close() throws InterruptedException{
+
+    public void close() throws InterruptedException {
         zooKeeper.close();
     }
+
+    public void process(WatchedEvent event) {
+        switch(event.getType()) {
+            case None:
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to zookeeper!!");
+                } else {
+                    synchronized (zooKeeper) {
+                        System.out.println("Disconnected from zookeeper event");
+                        zooKeeper.notifyAll();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void volunteerForLeadership() throws KeeperException, InterruptedException {
+        String zNodePrefix = ELECTION_NAMESPACE + "/c_";
+        String zNodeFullPath = zooKeeper.create(zNodePrefix, new byte[] {}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("znode_name: " + zNodeFullPath);
+        this.currentZnodeName = zNodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void electLeader() throws KeeperException,InterruptedException{
+        List<String> children=zooKeeper.getChildren(ELECTION_NAMESPACE,false);
+        Collections.sort(children);
+
+        String smallestChild=children.get(0);
+
+        if(smallestChild.equals(currentZnodeName)){
+            System.out.println("I am the leader");
+            return;
+        }
+        System.out.println("I am not the leader"+smallestChild+" is the leader");
+    }
+
+
 }
